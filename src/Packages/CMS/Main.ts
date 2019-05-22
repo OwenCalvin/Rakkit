338fbf5f466160eb3177ef0fbf647e65b894cf4f
Owen Gombas <owen.gombas@gmail.com>
2019-05-22 13:54:37 +0200

    improvement(final)

--- src/Packages/CMS/Main.ts
+++ src/Packages/CMS/Main.ts
@@ -10,7 +10,7 @@ export class Main {
       host: "localhost",
       port: 3306,
       username: "root",
-      password: "root",
+      password: "",
       database: "tsd",
       synchronize: true,
       entities: [
